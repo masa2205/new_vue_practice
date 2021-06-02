ae972cd08296bde62111a02a861b40d3b57a11c3
Masa <katsukatsu.masa.0522@gmail.com>
2021-06-02 10:01:48 +0900

    first commit

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-var obj = {
-    foo: 'bar'
-}
-
-Object.freeze(obj)
-
-const Layout = {
-    template: `
-        <div class="container">
-            <header>
-                <slot name="header"></slot>
-            </header>
-            <main>
-                <slot></slot>
-            </main>
-            <footer>
-                <slot name="footer"></slot>
-            </footer>
-        </div>        
-    `
-}
-
 new Vue ({
     el: '#app',
-    components: {
-        'layout': Layout
-    }
+    
 }) 
