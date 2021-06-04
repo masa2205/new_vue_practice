60555499552e139a3f8434d3cb0a89613be3dcd2
Masa <katsukatsu.masa.0522@gmail.com>
2021-06-04 19:05:12 +0900

    #57 ストレージへの保存の自動化

--- app.js
+++ app.js
@@ -2,5 +2,27 @@ new Vue ({
     el: '#app',
     data: {
         todos: []
+    },
+    methods: {
+        doAdd: function (event,value) {
+            let comment = this.$refs.comment
+            if (!comment.value.length) {
+                return
+            }
+            this.todos.push({
+                id: todoStorage.uid++,
+                comment: comment.value,
+                state: 0
+            })
+            comment=''
+        }
+    },
+    watch: {
+        todos: {
+            handler: function (todos) {
+                todoStorage.save(todos)
+            },
+            deep: true
+        }
     }
 }) 
